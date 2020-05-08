f6cb6ba21e64782a45c8e16fb311653d0b4801b1
luizzitol <62354883+luizzitol@users.noreply.github.com>
2020-05-08 22:01:52 +0100

    Updates

---
+++ precache-manifest.d3c2eeebed49c7336b20a64c20fc807a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657b279c43fcc0112ed0dca82f7e6e7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f745221d25a974e3f524",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "620e0e0cf401b9c1e086",
+    "url": "/static/css/main.205c3f6c.chunk.css"
+  },
+  {
+    "revision": "f745221d25a974e3f524",
+    "url": "/static/js/2.12458252.chunk.js"
+  },
+  {
+    "revision": "f73ad6c90ddc4ae1462794006f2b4023",
+    "url": "/static/js/2.12458252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620e0e0cf401b9c1e086",
+    "url": "/static/js/main.55caed3d.chunk.js"
+  },
+  {
+    "revision": "bd3077619dd7f0883e47",
+    "url": "/static/js/runtime-main.24b3ff52.js"
+  },
+  {
+    "revision": "ecb5db6d26a80a4197c04ea063328577",
+    "url": "/static/media/spinner.ecb5db6d.svg"
+  }
+]);
